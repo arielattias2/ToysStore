52b775ff5fddda61ead408fe98997aa4fd970c25
arielattias2 <arielattias2@gmail.com>
2023-09-20 19:55:21 +0300

    install path lib

--- startup/routes.js
+++ startup/routes.js
@@ -3,6 +3,7 @@ const users = require("../routes/users");
 const auth = require("../routes/auth");
 const express = require("express");
 const error = require("../middleware/error");
+const path = require("path");
 
 module.exports = (app) => {
   app.use(express.static(path.join(__dirname, "public"))); //Define public dir and it's files to be public
