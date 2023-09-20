9a213beafd5015de157fe9c8cb6c29360299b1c9
arielattias2 <arielattias2@gmail.com>
2023-09-20 20:30:10 +0300

    Add index Route

--- startup/routes.js
+++ startup/routes.js
@@ -1,13 +1,16 @@
 const toys = require("../routes/toys");
 const users = require("../routes/users");
+const indexRoute = require("../routes/indexRoute");
 const auth = require("../routes/auth");
 const express = require("express");
 const error = require("../middleware/error");
 const path = require("path");
 
 module.exports = (app) => {
-  app.use(express.static(path.join(__dirname, "public"))); //Define public dir and it's files to be public
+  app.use(express.static(path.join(__dirname, "../public"))); //Define public dir and it's files to be public
+
   app.use(express.json());
+  app.use("/", indexRoute);
   app.use("/toys", toys);
   app.use("/users", users);
   app.use("/users/login", auth);
