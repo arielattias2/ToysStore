f3a10a12a23771288b751d37d65ea5c6f2ad9993
arielattias2 <arielattias2@gmail.com>
2023-09-21 13:20:27 +0300

    a

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("express-async-errors"); // Using this to handle async errors (wrap each route)
 const express = require("express");
 const logger = require("./startup/logger");
-const { deleteAll, createToys } = require("./data/toys");
 
 const app = express();
 
